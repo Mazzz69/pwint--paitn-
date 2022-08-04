e67c1a258f73b2da811cc6eea45fe500c31be2ba
mazzz69 <marcel.maszewski@gmail.com>
2022-08-04 19:38:05 +0200

    first commit

---
+++ pwint.py
@@ -0,0 +1,26 @@
+"""
+@ Date: 08/04/2022
+@ Version: 1.0
+@ Lang: Pwython 3.9
+  """
+
+# passes
+def __pass__():
+	pass #pass
+
+def guesNum(num):
+	n = None
+	assert n == num
+
+def pwint(s):
+	return print(s) # weturns stwing
+                                                                # hi
+
+pwint("Pwint!")             #pwints the pwint 
+
+
+# hole wasser brrb | da bin ich doch glatt wasser holen gegangen ohne mein glas! ja sapperlot
+# jetzt kannst du dann mit ctrl+shift+g version control oeffnen und wir pushenS, das erstellt glaube ich auch schon das repository wenn ich mich richtig an letztes mal erinnere :D 
+# dann mach ich eine pr und schon hast du zwei commits :OO
+# ;)
+#oke
